e07705fe5efba2b18d6e5cbd8c6661efbdb1a34c
KiritoZweii <clindsey@atl.yearup.org>
2015-09-22 11:23:34 -0400

    I'm awesome!

---
+++ Eins/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My name is Corey.");
+		System.out.println("My favorite movie is Maxium Ride.");
+		System.out.println("She gives me buggers!");
+
+	}
+
+}
