4446f19511809c66771c730449defd1c7edff848
KiritoZweii <Clindsey@atl.yearup.org>
2015-09-26 15:17:47 -0400

    working with methods

---
+++ CalculateCircleArea/src/Area.java
@@ -0,0 +1,23 @@
+
+public class Area {
+	public static void main(String[] args) {
+		System.out.println("I shall tell you about myself...");
+		tellmeaboutyourself();
+		
+		response("Ko");
+	}
+	
+	static void response(String name) {
+		System.out.println("Hello, my name is " + name);
+	}
+	
+	static void tellmeaboutyourself() {
+		System.out.println("My name is Kelli.");
+		System.out.println("I like the color blue.");
+		System.out.println("I do makeup.");
+		
+	}
+
+
+	
+}
