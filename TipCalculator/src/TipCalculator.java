cfe849d48d32fa67ddb473153c6d02efbfad37bd
KiritoZweii <Clindsey@atl.yearup.org>
2015-10-04 13:52:24 -0400

    I did it!

---
+++ TipCalculator/src/TipCalculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class TipCalculator {
+	
+	static double chek;
+
+	public static void main(String[] args) {
+		System.out.println("What is your bill amount? ");
+		Scanner Amount = new Scanner(System.in);
+		chek = Amount.nextFloat();
+		System.out.println("Your total with 10 percent is: " + TipTen());
+		System.out.println("Your total with 15 percent is: " + TipFifthteen());
+		System.out.println("Your total with 20 percent is: " + TipTwenty());
+		System.out.println("Your total with 25 percent is: " + TipTwentyFive());
+		
+		
+		double tip10;
+		tip10 = TipTen();
+		double tip15;
+		tip15 = TipFifthteen();
+		double tip20;
+		tip20 = TipTwenty();
+		double tip25;
+		tip25 = TipTwentyFive();
+
+	}
+	
+	public static double TipTen()
+	{
+		double TipTen = chek * 10 / 100;
+		return TipTen;
+	}
+	
+	public static double TipFifthteen()
+	{
+		double TipFifthteen = chek * 15 / 100;
+		return TipFifthteen;
+	}
+	
+	public static double TipTwenty()
+	{
+		double TipTwenty = chek * 20 / 100;
+		return TipTwenty;
+	}
+	
+	public static double TipTwentyFive()
+	{
+		double TipTwentyFive = chek * 25 / 100;
+		return TipTwentyFive;
+	}
+	
+
+}
